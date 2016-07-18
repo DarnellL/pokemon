c52282bf3e3da749f70a2287ef4bef12a3e6ee9f
Amar Bhamra <amar.bhamra@wtg.co.uk>
2016-07-18 11:12:37 +0100

    Create template

---
+++ src/app/main.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    angular.module('pokemon', []);
+})();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5tb2R1bGUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uICgpIHtcclxuICAgICd1c2Ugc3RyaWN0JztcclxuXHJcbiAgICBhbmd1bGFyLm1vZHVsZSgncG9rZW1vbicsIFtdKTtcclxufSkoKTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
